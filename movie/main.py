b870bb3e026d5e25e855c5a81f973e0f36de8924
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-06-11 18:41:25 +0330

    read data

--- movie/main.py
+++ movie/main.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
 ratings = pd.read_csv("./data/u.data",sep="\t",names=['user_id','movie_id','rating','timestamp'])
-
-print(ratings.head())
+movies = pd.read_csv('./data/u.item',sep='|',encoding='latin-1',header=None,usecols=[0,1],names=['movie_id','title'])
+users = pd.read_csv('./data/u.user',sep='|',header=None,names=['user_id','age','gender','occupation','zip_code'])
