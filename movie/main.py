050fba9629c61f52ce1895da70901d228be76471
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-06-11 18:52:08 +0330

    update : implement recommedation correlation

--- movie/main.py
+++ movie/main.py
@@ -6,4 +6,29 @@ users = pd.read_csv('./data/u.user',sep='|',header=None,names=['user_id','age','
 
 # merge ragins + movives
 df = pd.merge(ratings,movies,on='movie_id')
-print(df.head())
+
+
+# movie-user matrix
+movie_matrix = df.pivot_table(
+    index='user_id',
+    columns='title',
+    values='rating'
+)
+
+# test 
+toy_story = movie_matrix['Toy Story (1995)']
+
+similar_movies = movie_matrix.corrwith(toy_story)
+
+corr_df = pd.DataFrame(similar_movies,columns=['correlation'])
+corr_df.dropna(inplace=True)
+
+# quality filter
+movie_stats = df.groupby('title')['rating'].agg(['mean','count'])
+
+recommendations = corr_df.join(movie_stats['count'])
+# filter recommendations
+recommendations = recommendations[recommendations['count'] > 100]
+
+rec_items = recommendations.sort_values('correlation',ascending=False).head(10)
+print(rec_items)
