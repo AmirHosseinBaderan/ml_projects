4b3643aa177c6afa0893f9ad99b2c82d54d69ce0
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-06-11 18:42:53 +0330

    update : merge data

--- movie/main.py
+++ movie/main.py
@@ -3,3 +3,7 @@ import pandas as pd
 ratings = pd.read_csv("./data/u.data",sep="\t",names=['user_id','movie_id','rating','timestamp'])
 movies = pd.read_csv('./data/u.item',sep='|',encoding='latin-1',header=None,usecols=[0,1],names=['movie_id','title'])
 users = pd.read_csv('./data/u.user',sep='|',header=None,names=['user_id','age','gender','occupation','zip_code'])
+
+# merge ragins + movives
+df = pd.merge(ratings,movies,on='movie_id')
+print(df.head())
