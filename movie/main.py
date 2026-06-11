58262ad9a1354d9f5b66770d6159229af6e15797
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-06-11 20:55:49 +0330

    Update main.py

--- movie/main.py
+++ movie/main.py
@@ -1,10 +1,9 @@
 from engine import build_engine
 from data import load_data
-from search import search
 
 df = load_data()
 
-recommender, movie_stats = build_engine(df)
+recommender,search_engine = build_engine(df)
 
 while True:
 
@@ -15,8 +14,15 @@ while True:
 
     elif cmd == "1":
         q = input("Search: ")
-        print(search(movie_stats, q))
+        results = search_engine.search(q)
 
+        if len(results) == 0:
+            print("No movies found")
+            continue
+        
+        for i, movie in enumerate(results.index, start=1):
+            row = results.loc[movie]
+            print(f"{i}. {movie} (rating={row['mean']:.2f}, votes={row['count']})")
     elif cmd == "2":
         movie = input("Movie: ")
         print(recommender.recommend(movie))
