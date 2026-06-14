11b574bb2d7615d5da2567a09712e8f53f091c6d
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-06-14 22:51:47 +0330

    fix : name of variables

--- knn-from-scratch/main.py
+++ knn-from-scratch/main.py
@@ -4,23 +4,23 @@ from knn import euclidean_distance,get_neighbors,predict
 df = pd.read_csv('./data/iris.csv')
 
 # X
-distance_df = df.drop(columns=['variety'])
+X = df.drop(columns=['variety'])
 y = df['variety']
 
-sample = distance_df.loc[1].values
-neighbors = get_neighbors(distance_df,y,sample,k=3)
+sample = X.iloc[1].values
+neighbors = get_neighbors(X,y,sample,k=3)
 
 print(neighbors)
 
-print(euclidean_distance(distance_df.loc[1].values,distance_df.loc[2].values))
+print(euclidean_distance(X.loc[1].values,X.loc[2].values))
 
-result = predict(distance_df,y,sample,k=3)
+result = predict(X,y,sample,k=3)
 print(result)
 
 current = 0
-for i in range(len(distance_df)):
-    pred = predict(distance_df,y,distance_df.iloc[i].values,k=3)
+for i in range(len(X)):
+    pred = predict(X,y,X.iloc[i].values,k=3)
     if pred == y.iloc[i]:
         current += 1
         
-print(f" Accuracy : {current / len(distance_df)}")
+print(f" Accuracy : {current / len(X)}")
