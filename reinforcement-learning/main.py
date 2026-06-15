0f92d080f1c9c2ee037111622cebacf3e4907d69
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-06-16 00:39:07 +0330

    update : implement train , run train test

--- reinforcement-learning/main.py
+++ reinforcement-learning/main.py
@@ -1,8 +1,13 @@
-import gym
+import gymnasium as gym
 import numpy as np
 import random
+import os
+from time import sleep
 
-env = gym.make('Taxi-v3',render_mode='ansi')
+def clear_terminal():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+env = gym.make('Taxi-v4',render_mode='ansi')
 state,info = env.reset()
 
 taxi = env.unwrapped
@@ -18,7 +23,7 @@ exploration = 0.1
 learning_rate = 0.1
 discount_factor = 0.6
 
-for tax_run in range(epochs):
+for taxi_run in range(epochs):
     state,info = env.reset()
     done = False
     
@@ -29,9 +34,27 @@ for tax_run in range(epochs):
         else:
             action = np.argmax(q_table[state])
             
-        next_state,reward,done,info = env.step(action)
+        next_state,reward,terminated,truncated,info = env.step(action)
+        done = terminated or truncated
         prev_q = q_table[state,action]
         next_max_q = np.max(q_table[next_state])
         new_q = (1 - learning_rate) * prev_q + learning_rate * (reward + discount_factor * next_max_q)
         q_table[state,action] = new_q
         state = next_state
+        
+for tripnum in range(1,11):
+    state,info = env.reset()
+    done = False
+    
+    while not done:
+        action = np.argmax(q_table[state])
+        next_state,reward,terminated,truncated,info = env.step(action)
+        done = terminated or truncated
+        clear_terminal()
+        print(f'trip number {tripnum}')
+        print(env.render())
+        sleep(.5)
+        state  = next_state
+    sleep(2)
+    
+    
