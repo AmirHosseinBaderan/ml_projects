7af99a4a69e4e6f2f8f23aa32aaf8c9206ce6528
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-06-15 23:05:02 +0330

    update : add details

--- reinforcement-learning/main.py
+++ reinforcement-learning/main.py
@@ -1,6 +1,18 @@
 import gym
+import numpy as np
 
 env = gym.make('Taxi-v3',render_mode='ansi')
 state,info = env.reset()
 
+taxi = env.unwrapped
+init_state = taxi.encode(2,3,2,0)
+taxi.s = init_state
+
 print(env.render())
+
+q_table = np.zeros([env.observation_space.n,env.action_space.n])
+
+epochs = 10000
+exploration = 0.1
+learning_rate = 0.1
+discount_rate = 0.6
