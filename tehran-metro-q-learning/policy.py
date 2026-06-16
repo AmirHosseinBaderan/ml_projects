59822c7dab64ad8298acd0683e85e4e8eea3d1d9
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-06-17 00:12:37 +0330

    delete : project

--- tehran-metro-q-learning/policy.py
+++
@@ -1,9 +0,0 @@
-import random
-
-def choose_action(state,q_table,epsilon):
-    actions = q_table[state]
-    
-    if random.random() < epsilon:
-        return random.choice(list(actions.keys()))
-    
-    return max(actions,key=actions.get)
