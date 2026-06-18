c8a4217ad099ee255954320630100ed609b7a7c9
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-06-18 21:48:57 +0330

    check and debug

--- dog_cat/main.py
+++ dog_cat/main.py
@@ -3,12 +3,15 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 from model_plt import show_model_plot
 import os
+from predictor import predict_image
 
 MODEL_PATH = "cats_vs_dogs.keras"
 
 base_dir = Path('./data')
 cat_dir = base_dir / 'train/cats/'
 dog_dir = base_dir / 'train/dogs/'
+cat_test_dir = base_dir / 'validation/cats/'
+dog_test_dir = base_dir / 'validation/dogs/'
 
 train_cats = len(list(cat_dir.glob('*')))
 train_dogs = len(list(dog_dir.glob('*')))
@@ -46,20 +49,18 @@ def build_model():
     return tf.keras.Sequential([
         normalization_layer,
 
-        tf.keras.layers.Conv2D(32, 3, activation='relu'),
+        tf.keras.layers.Conv2D(32,3,activation='relu'),
         tf.keras.layers.MaxPooling2D(),
 
-        tf.keras.layers.Conv2D(64, 3, activation='relu'),
+        tf.keras.layers.Conv2D(64,3,activation='relu'),
         tf.keras.layers.MaxPooling2D(),
 
-        tf.keras.layers.Conv2D(128, 3, activation='relu'),
+        tf.keras.layers.Conv2D(128,3,activation='relu'),
         tf.keras.layers.MaxPooling2D(),
 
-        tf.keras.layers.GlobalAveragePooling2D(),
+        tf.keras.layers.Flatten(),
 
-        tf.keras.layers.Dense(256, activation='relu'),
-
-        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(512, activation='relu'),
 
         tf.keras.layers.Dense(1, activation='sigmoid')
     ])
@@ -111,3 +112,31 @@ if history is not None:
     print(max(history.history['val_accuracy']))
 
     show_model_plot(history)
+    
+for images, labels in val_ds.take(1):
+    preds = model.predict(images)
+
+    print("raw predictions:")
+    print(preds[:10])
+
+    print("labels:")
+    print(labels.numpy()[:10])
+
+    
+print("Check dogs image")
+wrong_dogs = 0
+for d in range(len(list(dog_test_dir.glob('*')))):
+    predict,lable = predict_image(list(dog_test_dir.glob('*'))[d],model,['cats','dogs'])
+    if lable != 'dogs':
+        wrong_dogs +=1 
+
+print(f"Wrong dogs detection : {wrong_dogs}")
+
+print("Check cats image")
+wrong_cats = 0
+for d in range(len(list(cat_test_dir.glob('*')))):
+    predict,lable = predict_image(list(cat_test_dir.glob('*'))[d],model,['cats','dogs'])
+    if lable != 'cats':
+        wrong_cats +=1 
+
+print(f"Wrong cats detection : {wrong_cats}")
--- dog_cat/predictor.py
+++ dog_cat/predictor.py
@@ -3,21 +3,14 @@ import numpy as np
 
 IMG_SIZE = (150,150)
 
-def predict_image(image_path,model):
-    img = tf.keras.utils.load_img(
-        image_path,
-        target_size=IMG_SIZE
-    )
-    
+def predict_image(image_path, model, class_names):
+    img = tf.keras.utils.load_img(image_path, target_size=(150,150))
     img_array = tf.keras.utils.img_to_array(img)
-    img_array = img_array / 255 # normalize
-    img_array = np.expand_dims(img_array,axis=0) # (1,150,150,3)
-    
-    prediction = model.predict(img_array,verbose=0)[0][0]
-    if prediction > 0.5:
-        label = "Dog"
-    else:
-        label = "Cat"
-    
-    print(f"Prediction Score : {prediction:.4f}")
-    print(f"Result {label}")
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = img_array / 255.0
+
+    prediction = model.predict(img_array, verbose=0)[0][0]
+
+    label = class_names[int(prediction > 0.5)]
+
+    return prediction, label
