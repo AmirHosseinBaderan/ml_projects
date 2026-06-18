0d1c905783de16fefb373e696614cdc99be1f3fb
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-06-18 16:23:01 +0330

    create : tensorflow mnist

---
+++ tensorflow-mnist/main.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+from tensorflow.keras.datasets import mnist 
+from tensorflow.keras.utils import to_categorical
+
+# load dataset
+(x_train,y_train),(x_test,y_test) = mnist.load_data()
