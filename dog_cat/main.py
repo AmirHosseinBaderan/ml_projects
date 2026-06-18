51e381a2b4a6d5ff5a0201dfe0c9832a7e975199
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-06-18 18:06:14 +0330

    update : load and show data

--- dog_cat/main.py
+++ dog_cat/main.py
@@ -1,9 +1,26 @@
 from pathlib import Path
+import matplotlib.pyplot as plt
 
-base_dir = Path('./data').parent
+base_dir = Path('./data')
+cat_dir = base_dir / 'train/cats/'
+dog_dir = base_dir / 'train/dogs/'
 
-train_cats = len(list((base_dir / '/train/cats/').glob('*')))
-train_dogs = len(list((base_dir / '/train/dogs/').glob('*')))
+train_cats = len(list(cat_dir.glob('*')))
+train_dogs = len(list(dog_dir.glob('*')))
 
 print(f"Cats : {train_cats}")
 print(f"Dogs : {train_dogs}")
+
+
+images = list(cat_dir.glob("*.jpg"))
+plt.figure(figsize=(10,0))
+
+for i in range(9):
+    plt.subplot(3,3,i+1)
+    
+    img = plt.imread(images[i])
+    plt.imshow(img)
+    plt.axis("off")
+    
+plt.show()
+    
