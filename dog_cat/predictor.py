2760b4f59103911528beda7cfe1c746fec779b5c
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-06-19 00:00:08 +0330

    update : make predict function

--- dog_cat/predictor.py
+++ dog_cat/predictor.py
@@ -1,16 +1,23 @@
-import tensorflow as tf
+import cv2
 import numpy as np
 
-IMG_SIZE = (150,150)
-
-def predict_image(image_path, model, class_names):
-    img = tf.keras.utils.load_img(image_path, target_size=(150,150))
-    img_array = tf.keras.utils.img_to_array(img)
-    img_array = np.expand_dims(img_array, axis=0)
-    img_array = img_array / 255.0
-
-    prediction = model.predict(img_array, verbose=0)[0][0]
-
-    label = class_names[int(prediction > 0.5)]
-
-    return prediction, label
+def predict_image(model,image_path,img_size,class_indices):
+    # invert mapping 
+    classes = {v: k for k, v in class_indices.items()}
+    
+    # read image
+    
+    image = cv2.imread(image_path)
+    image = cv2.resize(image, img_size)
+    image = np.expand_dims(image, axis=0)
+    image = image / 255.0
+    
+    # prediction
+    prediction = model.predict(image)[0][0]
+    print('raw prediction : ',prediction)
+    
+    # convert to class
+    class_index = int(prediction > 0.5)
+    result = classes[class_index]
+    
+    return result,prediction
