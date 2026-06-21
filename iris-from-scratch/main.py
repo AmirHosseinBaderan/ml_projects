96b067a797bd2296436b3d88355e14dd6085d77a
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-06-21 21:24:52 +0330

    split train test data

--- iris-from-scratch/main.py
+++ iris-from-scratch/main.py
@@ -1,7 +1,22 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv("./data/iris.csv")
 
 # Features , Label
 X = df.drop(columns=["variety"])
 y = df["variety"]
+
+# train test split
+X_train,X_test,y_train,y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
+print(X_train.shape)
+print(X_test.shape)
+
+print(y_train.shape)
+print(y_test.shape)
