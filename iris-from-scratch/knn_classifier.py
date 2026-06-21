00a156931cdf677570530f7fe9d827f1df9707ad
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-06-21 22:01:18 +0330

    update : implement knn classifier

--- iris-from-scratch/knn_classifier.py
+++ iris-from-scratch/knn_classifier.py
@@ -10,3 +10,40 @@ def euclidean_distance(x, y):
 
     return math.sqrt(sum_squared)
 
+class KNNClassifier:
+    def __init__(self, k=3):
+        self.y_train = None
+        self.x_train = None
+        self.k = k
+
+    def fit(self,x_train,y_train):
+        self.x_train = x_train
+        self.y_train = y_train
+
+    def predict(self,sample):
+        items = []
+        for i in range(len(self.x_train)):
+            x_sample = self.x_train[i]
+            y_sample = self.y_train[i]
+            distance = euclidean_distance(x_sample, sample)
+            items.append((distance,y_sample))
+
+        items.sort(key=lambda x: x[0])
+        neighbors = items[:self.k]
+
+        votes = {}
+        for _,label in neighbors:
+            if votes.get(label) is None:
+                votes[label] = 1
+            else:
+                votes[label] += 1
+
+        predict = ''
+        largest = 0
+        for key in votes.keys():
+            value = votes[key]
+            if value > largest:
+                largest = value
+                predict = key
+
+        return predict
