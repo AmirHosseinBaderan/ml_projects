09f0e7eee51b179d142ffb8365ce88e76ca0b21c
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-06-21 21:20:50 +0330

    split features labels

--- iris-from-scratch/main.py
+++ iris-from-scratch/main.py
@@ -2,9 +2,6 @@ import pandas as pd
 
 df = pd.read_csv("./data/iris.csv")
 
-print(df.head())
-
-print(df.shape)
-print(df.columns)
-print(df.info())
-print(df.describe())
+# Features , Label
+X = df.drop(columns=["variety"])
+y = df["variety"]
