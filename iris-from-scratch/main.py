61e6e40cf3fce25ba170a15f73c7418ca8ac9bbd
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-06-21 22:01:27 +0330

    update : move function to classifier

--- iris-from-scratch/main.py
+++ iris-from-scratch/main.py
@@ -16,18 +16,3 @@ X_train,X_test,y_train,y_test = train_test_split(
     random_state=42
 )
 
-# distance
-def euclidean_distance(x, y):
-
-    n = len(x)
-    sum_squared = 0
-    for i in range(n):
-        sum_squared += (x[i] - y[i]) ** 2
-
-    return math.sqrt(sum_squared)
-
-a = [5.1, 3.5, 1.4, 0.2]
-b = [5.2, 3.4, 1.5, 0.3]
-c = [7.0, 3.2, 5.9, 2.1]
-print(euclidean_distance(a,b))
-print(euclidean_distance(a, c))
