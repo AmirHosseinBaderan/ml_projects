c85325170092262fb1a46353a6d1e718a387b204
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-06-23 00:51:00 +0330

    update : make feature sampler

--- random-forest-scratch/sample.py
+++ random-forest-scratch/sample.py
@@ -4,3 +4,7 @@ def bootstrap_sample(X,y):
     n_samples = X.shape[0]
     idxs = np.random.choice(n_samples,n_samples,replace=True)
     return X[idxs], y[idxs]
+
+def random_feature_subset(X,n_features):
+    feature_idx = np.random.choice(X.shape[1],n_features,replace=False)
+    return feature_idx
