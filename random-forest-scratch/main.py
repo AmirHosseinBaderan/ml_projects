86a8c9224b4a12caab48d9710daa8c9bbfcc245c
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-06-22 22:43:08 +0330

    Update main.py

--- random-forest-scratch/main.py
+++ random-forest-scratch/main.py
@@ -1,8 +1,7 @@
 import pandas as pd
 import numpy as np
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
-from collections import Counter
+from forest import RandomForest
 
 df = pd.read_csv("./data/iris.csv")
 X = df.drop(columns=['variety']).values
@@ -11,3 +10,15 @@ y = df['variety'].values
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
+
+print(X_train.shape, X_test.shape)
+print(y_train.shape, y_test.shape)
+
+model = RandomForest(n_trees=100,max_depth=10)
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+acc = np.mean(y_pred == y_test)
+
+print(f"Accuracy : {acc}")
