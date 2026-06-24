2763e158e95b8ce70a46858315037361826641d8
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-06-24 22:06:28 +0330

    update : implement new items

--- k_means_from_scratch/k_mean.py
+++ k_means_from_scratch/k_mean.py
@@ -1,6 +1,7 @@
 import math
 import random
 
+
 def euclidean_distance(p1, p2):
     total = 0
 
@@ -9,12 +10,43 @@ def euclidean_distance(p1, p2):
 
     return math.sqrt(total)
 
+
 class KMeans:
-    def __init__(self,k=2,max_iters=100):
+    def __init__(self, k=2, max_iters=100):
         self.k = k
         self.max_iters = max_iters
         self.centroids = []
 
-    def init_centroids(self,X):
-        self.centroids = random.sample(X,self.k)
+    def init_centroids(self, X):
+        self.centroids = random.sample(X, self.k)
 
+    def assign_clusters(self, X):
+        clusters = [[] for _ in range(self.k)]
+
+        for point in X:
+            distances = []
+            for centroid in self.centroids:
+                dist = euclidean_distance(
+                    point,
+                    centroid
+                )
+
+                distances.append(dist)
+
+            closest_idx = distances.index(
+                min(distances)
+            )
+            clusters[closest_idx].append(point)
+
+        return clusters
+
+    def update_centroids(self, clusters):
+        centroids = []
+        for cluster in clusters:
+            centroid = []
+
+            for feature_value in zip(*cluster):
+                mean = sum(feature_value) / len(feature_value)
+                centroid.append(mean)
+            centroids.append(centroid)
+        self.centroids = centroids
