f3664a024671f1a8eea096a09e0824baac98d018
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-06-27 00:06:50 +0330

    Update main.py

--- DBSCAN_sklearn/main.py
+++ DBSCAN_sklearn/main.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from sklearn.cluster import DBSCAN
 
 X = [
     [1, 2],
@@ -13,5 +14,24 @@ x = [p[0] for p in X]
 y = [p[1] for p in X]
 
 plt.scatter(x,y)
+plt.grid(True)
+plt.show()
+
+model = DBSCAN(
+    eps=20,
+    min_samples=2,
+)
+
+labels = model.fit_predict(X)
+print(labels)
+
+# show result
+plt.scatter(
+    x,
+    y,
+    c=labels,
+    s=100,
+)
+
 plt.grid(True)
 plt.show()
