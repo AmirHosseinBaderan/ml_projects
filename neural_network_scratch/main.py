c722f03216004fcdd835b0d960141a50af5ca18c
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-06-29 01:01:36 +0330

    update : implement first network

--- neural_network_scratch/main.py
+++ neural_network_scratch/main.py
@@ -1,10 +1,29 @@
-from neuron import Neuron
+from network import Network
+from  layer import Layer
 
-neuron = Neuron(
-    weights=[0.4, 0.6],
-    bias=1
+network = Network()
+
+# add layers
+network.add(
+    Layer(
+        neuron_count=4,
+        input_size=2
+    )
 )
 
-result = neuron.forward([2, 5])
+network.add(
+    Layer(
+        neuron_count=2,
+        input_size=4
+    )
+)
 
+network.add(
+    Layer(
+        neuron_count=1,
+        input_size=2
+    )
+)
+
+result = network.predict([1000, -500])
 print(result)
