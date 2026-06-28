e353750d59274dd392e79481c9d58f9026cc9f45
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-06-29 00:42:01 +0330

    Update network.py

--- neural_network_scratch/network.py
+++ neural_network_scratch/network.py
@@ -1,5 +1,28 @@
+from layer import Layer
+from module import Module
 
-class Network:
+class Network(Module):
 
     def __init__(self):
         self.layers = []
+        self.inputs = None
+        self.outputs = None
+
+    def add(self,layer):
+        if not isinstance(layer, Layer):
+            raise TypeError("layer must be an instance of Layer")
+
+        self.layers.append(layer)
+
+    def forward(self,inputs):
+        self.inputs = inputs
+        outputs = inputs
+
+        for layer in self.layers:
+            outputs = layer.forward(outputs)
+
+        self.outputs = outputs
+        return outputs
+
+    def predict(self,inputs):
+        return self.forward(inputs)
