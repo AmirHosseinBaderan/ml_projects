078a17062a20744651c9221a3d492a428f29c1a2
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-06-29 01:15:56 +0330

    update : make sigmoid

--- neural_network_scratch/activations.py
+++ neural_network_scratch/activations.py
@@ -1,12 +1,26 @@
 from abc import ABC,abstractmethod
-
+import math
 
 class Activation(ABC):
     @abstractmethod
     def activate(self,x):
         pass
 
+    @abstractmethod
+    def derivative(self, output):
+        pass
+
 class Identity(Activation):
 
     def activate(self,x):
         return x
+
+    def derivative(self, output):
+        return 1
+
+class Sigmoid(Activation):
+    def activate(self,x):
+        return 1 / (1 + math.exp(-x))
+
+    def derivative(self, output):
+        return output * (1 - output)
