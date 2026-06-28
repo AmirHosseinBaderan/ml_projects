076425ac21479e435113baf674b539e008858e87
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-06-28 22:00:06 +0330

    Update main.py

--- neuron/main.py
+++ neuron/main.py
@@ -1,16 +1,16 @@
-x = [5, 8, 2]
-w = [0.5, -0.4, 2]
-b = 3
+x = [10, 20, 30, 40]
+w = [0.1, 0.2, 0.3, -0.5]
+b = 5
 
 
 def neuron(inputs,weights,bias):
-    weighted_sum = 0
-
-    for i in range(len(inputs)):
-        weighted_sum += inputs[i] * weights[i]
-
+    weighted_sum = sum(
+        x * w
+        for x, w in zip(inputs, weights)
+    )
     z = weighted_sum + bias
-    return z
 
-output = neuron(x, w, b)
-print(output)
+    return z,weighted_sum
+
+z,weighted_sum = neuron(x, w, b)
+print(f'Z : {z} / WS {weighted_sum}')
