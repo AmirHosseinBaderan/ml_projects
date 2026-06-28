df672bf77814fa59941735c4a69d7417e54b99a5
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-06-29 00:28:37 +0330

    update : implement neuron

---
+++ neural_network_scratch/neuron.py
@@ -0,0 +1,40 @@
+import random
+from module import Module
+from activations import Identity
+
+
+class Neuron(Module):
+    def __init__(
+            self,
+            input_size,
+            bias,
+            activation=Identity()
+    ):
+        self.weights = [
+            random.random()
+            for _ in range(input_size)
+        ]
+        self.bias = bias
+        self.activation = activation
+
+        self.inputs = None
+        self.z = None
+        self.output = None
+
+    def forward(self, inputs):
+        self.inputs = inputs
+
+        self.z = self._calculate_z(inputs)
+        self.output = self.activation.activate(self.z)
+
+        return self.output
+
+    def _calculate_z(self, inputs):
+
+        weighted_sum = 0
+
+        for x, w in zip(inputs, self.weights):
+            weighted_sum += x * w
+
+        return weighted_sum + self.bias
+
