ebe9eca8b9890e6dce6c48dc04e84f49f69781df
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-06-29 00:38:25 +0330

    Update layer.py

--- neural_network_scratch/layer.py
+++ neural_network_scratch/layer.py
@@ -1,5 +1,26 @@
+from neuron import Neuron
+from module import Module
 
-
-class Layer:
-    def __init__(self):
+class Layer(Module):
+    def __init__(self,neuron_count,input_size):
         self.neurons = []
+
+        self.inputs = None
+        self.outputs = None
+
+        for _ in range(neuron_count):
+            self.neurons.append(
+                Neuron(input_size)
+            )
+
+    def forward(self,inputs):
+        self.inputs = inputs
+        outputs = []
+
+        for neuron in self.neurons:
+            outputs.append(
+                neuron.forward(inputs)
+            )
+
+        self.outputs = outputs
+        return outputs
