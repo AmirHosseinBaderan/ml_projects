8e358f7dc7499c61c8dc1a80eff07482e18508b7
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-06-29 23:33:42 +0330

    Update optimizers.py

--- neural_network_scratch/optimizers.py
+++ neural_network_scratch/optimizers.py
@@ -1,2 +1,16 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
+
+class Optimizer(ABC):
+
+    @abstractmethod
+    def step(self, network):
+        pass
+
+class SGD(Optimizer):
+
+    def __init__(self, learning_rate=0.01):
+        self.learning_rate = learning_rate
+
+    def step(self, network):
+        pass
