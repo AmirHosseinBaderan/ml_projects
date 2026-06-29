9af95b358f737e3e4421efeda8e892799c0aeed0
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-06-29 22:30:56 +0330

    update : implement loss

---
+++ neural_network_scratch/losses.py
@@ -0,0 +1,51 @@
+from abc import ABC, abstractmethod
+
+
+class Loss(ABC):
+    def __init__(self):
+        self.prediction = None
+        self.target = None
+        self.value = None
+
+    @abstractmethod
+    def forward(self, prediction, target):
+        pass
+
+    @abstractmethod
+    def derivative(self):
+        pass
+
+
+class MSE(Loss):
+
+    def forward(self, prediction, target):
+        self.prediction = prediction
+        self.target = target
+
+        total = 0
+
+        for p, t in zip(prediction, target):
+            total += (p - t) ** 2
+
+        loss = total / len(prediction)
+        self.value = loss
+        return loss
+
+    def derivative(self):
+        gradients = []
+
+        n = len(self.prediction)
+
+        for p,t in zip(self.prediction, self.target):
+            gradients.append(
+                (2 * (p - t) / n)
+            )
+
+        return gradients
+
+class BinaryCrossEntropy(Loss):
+    def forward(self, prediction, target):
+        pass
+
+    def derivative(self, prediction, target):
+        pass
