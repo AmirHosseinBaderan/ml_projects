06c9e256484d843d5064ef5f52254146da75a522
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-06-29 20:57:36 +0330

    update : implement tanh

--- neural_network_scratch/activations.py
+++ neural_network_scratch/activations.py
@@ -24,3 +24,14 @@ class Sigmoid(Activation):
 
     def derivative(self, output):
         return output * (1 - output)
+
+
+class Tanh(Activation):
+    def activate(self,x):
+        # numerator = math.exp(x) - math.exp(-x)
+        # denominator = math.exp(x) + math.exp(-x)
+        # return numerator / denominator
+        return math.tanh(x)
+
+    def derivative(self, output):
+        return 1 - output ** 2
